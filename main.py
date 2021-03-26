1ad2700eb8a6ea93cb37efe00ccbe8ce405d7cae
recreationx <nanzheng03@gmail.com>
2021-03-27 01:59:55 +0800

    added auto coordinate retrieval

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from datastore import Datastore
 
-data_creation = Datastore("busdata.db")
-data_creation.init_all()
+data = Datastore("busdata.db")
+data.init_all()
 
 app = Flask(__name__)
 
@@ -14,6 +14,24 @@ def main():
 
 @app.route("/nearestbusstop", methods=["GET", "POST"])
 def nearestbusstop():
+    """Find the nearest bus stop from a given coordinate
+    Coordinate can be retrieved automatically or
+    manually entered
+
+    1. Take in given coordinate
+
+    TODO: implement AJAX for static loading
+    """
+    conn = data.get_conn()
+    cur = conn.cursor()
+    cur.execute(
+        """
+        SELECT * FROM busstops
+        """
+    )
+    a = cur.fetchall()
+    print(a)
+
     return render_template("nearestbusstop.html")
 
 
